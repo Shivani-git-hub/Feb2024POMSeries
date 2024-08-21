b436f2c8e65140e4833d3d0fa5fa05dc210fd64b
ShivaniMadagoni <madagonishivani3@gmail.com>
2024-08-22 00:48:51 +0530

    f3 feature

--- src/main/java/com/qa/opencart/pages/Feature.java
+++ src/main/java/com/qa/opencart/pages/Feature.java
@@ -5,6 +5,9 @@ public class Feature {
 	public static void main(String[] args) {
 
 		System.out.println("f1 feature");
+		System.out.println("f2 feature");
+		System.out.println("f3 feature");
+
 	}
 
 }
