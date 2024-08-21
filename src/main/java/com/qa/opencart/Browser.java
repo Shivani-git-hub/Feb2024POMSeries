437a156bc9119a3d46139c4e0bc764d85857b287
Shivani-git-hub <madagonishivani3@gmail.com>
2024-08-21 18:23:21 +0530

    Update Browser.java

--- src/main/java/com/qa/opencart/Browser.java
+++ src/main/java/com/qa/opencart/Browser.java
@@ -3,7 +3,8 @@ package com.qa.opencart.pages;
 public class Browser {
 	int p = 10;
 	public void login() {
-		System.out.println("naveen -- login");
+		System.out.println("Tom -- login");
+		System.out.println("Tom -- chrome");
 	}
 
 }
